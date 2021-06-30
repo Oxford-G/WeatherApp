5d8c9f0e3f3d248288c6af9863c6ecadfdafe80b
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-30 19:26:27 +0100

    add function setAction

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello webpack!")
+import './css/style.css';
+import getResult from './js/weather';
+import displayWeather from './js/background';
+import changeTemp from './js/temp';
+
+const search = document.querySelector('.search-box');
+
+function setAction(e) {
+  if (e.keyCode === 13) {
+    getResult(search.value)
+      .then((data) => displayWeather(data))
+      .then(search.value = '');
+  }
+}
+search.addEventListener('keypress', setAction);
+changeTemp();
